272ee0766dd77ecdc15b4b61fdd3ef55de1994c7
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-11-21 18:26:19 +0700

    menambah val nim string di mahasiswa

--- app/src/main/java/com/example/questnavigasimvvm_030/model/Mahasiswa.kt
+++ app/src/main/java/com/example/questnavigasimvvm_030/model/Mahasiswa.kt
@@ -2,7 +2,7 @@ package com.example.questnavigasimvvm_030.model
 
 data class Mahasiswa(
     val nama: String = "",
-    val nim: String = "",
+    val nim: String = "",//menambah val nim
     val gender: String = "",
     val alamat: String = "",
     val email: String = "",
