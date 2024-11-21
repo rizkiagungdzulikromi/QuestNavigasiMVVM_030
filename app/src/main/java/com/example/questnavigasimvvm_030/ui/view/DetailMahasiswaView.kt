ff32a6ffa8b6e0a176e51dd47281c2dc1713316b
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-11-21 18:29:42 +0700

    menambah textfield nim dan var nim

--- app/src/main/java/com/example/questnavigasimvvm_030/ui/view/DetailMahasiswaView.kt
+++ app/src/main/java/com/example/questnavigasimvvm_030/ui/view/DetailMahasiswaView.kt
@@ -22,3 +22,47 @@ fun DetailMahasiswaView(
     val listDataMhs = listOf(
         Pair("Nama", dataMhs.nama),
         Pair("Nim", dataMhs.nim),
+        Pair("Gender", dataMhs.gender),
+        Pair("Alamat", dataMhs.alamat),
+        Pair("Email", dataMhs.email),
+        Pair("NoTelpon", dataMhs.notelepon),
+    )
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Tombol kembali
+        ElevatedButton(
+            onClick = { onBackButton() } // Memanggil callback tombol kembali
+        ) {
+            Text("Kembali")
+        }
+
+        // Menampilkan detail mahasiswa
+        listDataMhs.forEach { data ->
+            DetailMhs(judul = data.first, isi = data.second)
+        }
+    }
+}
+
+
+@Composable
+fun DetailMhs(
+    judul: String, isi: String
+) {
+    Row(
+        modifier = Modifier.
+        fillMaxWidth().
+        padding(top = 10.dp)
+    ) {
+        Text(text = judul,
+            modifier = Modifier.weight(0.8f))
+        Text(text = ":",
+            modifier = Modifier.weight(0.2f))
+        Text(text = isi,
+            modifier = Modifier.weight(2f))
+    }
+
+}
